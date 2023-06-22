a6617c1936177325e918a49f3fb4b97f4e0f5d53
Anna Groshkova <anna.matvyeyenko@gmail.com>
2023-06-22 21:25:50 +0200

    the rest

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
+let teamOneButtons = document.querySelectorAll('.team__add-points-button_team-one');
+let teamTwoButtons = document.querySelectorAll('.team__add-points-button_team-two');
+
+let teamOne = document.querySelector('.team__points_team-one');
+let teamTwo = document.querySelector('.team__points_team-two');
+
 let scoreTeamOne = 0;
 let scoreTeamTwo = 0;
-//document.querySelector('.team__points_team-one').textContent = scoreTeamOne;
-//document.querySelector('.team__points_team-two').textContent = scoreTeamTwo;
 
 
-function addPointsTeamOne() {
-    document.querySelectorAll('.team__add-points-button_team-one').forEach(function(element) {
+
+function addPoints(addScoreButton, teamScore, team) {
+    addScoreButton.forEach(function(element) {
         element.addEventListener('click', function() {
             let addPoints = Number(element.textContent);
-            scoreTeamOne += addPoints;
-            document.querySelector('.team__points_team-one').textContent = scoreTeamOne;
-
+            teamScore += addPoints;
+            team.textContent = teamScore;
         })
     })
 }
 
-function addPointsTeamTwo() {
-    document.querySelectorAll('.team__add-points-button_team-two').forEach(function(element) {
-        element.addEventListener('click', function() {
-            let addPoints = Number(element.textContent);
-            scoreTeamTwo += addPoints;
-            document.querySelector('.team__points_team-two').textContent = scoreTeamTwo;
-
-        })
-    })
-}
-
-addPointsTeamOne()
-addPointsTeamTwo()
+addPoints(teamOneButtons, scoreTeamOne, teamOne)
+addPoints(teamTwoButtons, scoreTeamTwo, teamTwo)
